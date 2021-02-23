21bcc53a01799da730ae0ac45dd4ec7460c53051
lamsalkamal <ujstha27@gmail.com>
2021-02-23 16:32:20 +0200

    config change

--- function/config.php
+++ function/config.php
@@ -3,10 +3,10 @@
 	// define('DB_USER', 'iww4pw6bd59h');
 	// define('DB_PASS', '123Nexus#@!');
 	// define('DB_NAME', 'i6357113_wp2');
-	define('DB_SERVER', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', '');
-	define('DB_NAME', 'portfolio');
+	define('DB_SERVER', 'remotemysql.com');
+	define('DB_USER', 'qjoy4iyjsW');
+	define('DB_PASS', '0kQvld0Ne9');
+	define('DB_NAME', 'qjoy4iyjsW');
 	$conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 	// Check connection
 	if (mysqli_connect_errno()) {
