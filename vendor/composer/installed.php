fa3332c5a654d82d3bbab8a8af7768db37eea7b5
lamsalkamal <ujstha27@gmail.com>
2021-02-23 16:15:38 +0200

    (procfile change)

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '7ff29498463cacac3fc88fa02c8301f73ff7bb07',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7ff29498463cacac3fc88fa02c8301f73ff7bb07',
+    ),
+  ),
+);
